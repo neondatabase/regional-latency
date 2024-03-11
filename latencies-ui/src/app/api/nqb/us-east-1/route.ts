5d6298957f97da09798903d00e3fb55e756a7245
Evan Shortiss <evanshortiss@gmail.com>
2024-03-11 13:13:15 -0700

    fix: nqb platform detection

--- latencies-ui/src/app/api/nqb/us-east-1/route.ts
+++ latencies-ui/src/app/api/nqb/us-east-1/route.ts
@@ -5,6 +5,10 @@ export const dynamic = "force-dynamic";
 
 export async function GET(request: NextRequest) {
   const { runner, platform } = getBenchmarkInstance({
+    // Required to identify this as a vercel environment
+    ...process.env,
+
+    // Hacks to force NQB environment settings
     NQB_DATABASE_URL: process.env.NEON_DB_US_EAST_1,
     NQB_API_KEY: process.env.NQB_API_KEY
   })
