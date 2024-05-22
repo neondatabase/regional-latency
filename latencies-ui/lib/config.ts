86664c604bf99215c8cb8d33578c5a9ca1b1f0c3
Evan Shortiss <evanshortiss@gmail.com>
2024-05-21 17:56:13 -0700

    fix: remove unused api key variable

--- latencies-ui/lib/config.ts
+++ latencies-ui/lib/config.ts
@@ -9,7 +9,6 @@ export function getConfig (env: NodeJS.ProcessEnv): ApplicationConfig {
   const { get } = from(env)
   
   return {
-    API_KEY: get('API_KEY').required().asString(),
     BENCHMARK_QUERY_COUNT: get('BENCHMARK_QUERY_COUNT').default('10').asIntPositive(),
   }
 }
