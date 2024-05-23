d95e165c285fed6bdb664431bc258eb90063bbfa
Evan Shortiss <evanshortiss@gmail.com>
2024-05-23 13:23:34 -0700

    fix: layout exports

--- latencies-ui/src/app/layout.tsx
+++ latencies-ui/src/app/layout.tsx
@@ -3,7 +3,7 @@ import { IBM_Plex_Sans, Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-export const sans = IBM_Plex_Sans({
+const sans = IBM_Plex_Sans({
   display: 'swap',
   variable: '--font-sans',
   subsets: ['latin'],
