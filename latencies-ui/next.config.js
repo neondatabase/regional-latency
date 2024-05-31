dbba3ae00a98339331b8fc921f32fc1f60457991
Evan Shortiss <evanshortiss@gmail.com>
2024-05-31 10:06:11 -0700

    feat: serve from a base path in production

--- latencies-ui/next.config.js
+++ latencies-ui/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+
+const isProd = process.env.NODE_ENV === 'production'
+
+const nextConfig = {
+  basePath: isProd ? '/regional-latencies' : undefined
+}
 
 module.exports = nextConfig
