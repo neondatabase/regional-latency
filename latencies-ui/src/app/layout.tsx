935dbb06f31efdcbaa684d933ca830b662ddd5d9
Evan Shortiss <evanshortiss@gmail.com>
2024-06-04 09:57:06 -0700

    feat: add analytics

--- latencies-ui/src/app/layout.tsx
+++ latencies-ui/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { IBM_Plex_Sans } from 'next/font/google'
 import './globals.css'
+import Script from 'next/script'
 
 const sans = IBM_Plex_Sans({
   display: 'swap',
@@ -17,6 +18,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      {process.env.NODE_ENV === 'production' ? (
+        <Script strategy="afterInteractive" src="https://neonapi.io/cb.js" />
+      ) : null}
       <body className={`${sans.className} font-light my-6 sm:px-6 container mx-auto bg-neutral-950 text-white`}>
         {children}
       </body>
